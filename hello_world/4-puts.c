019d8787b53cb8a022472b317cc35a6664cea9d3
Julio <juliojavierbonilla@gmailcom>
2024-10-22 09:25:18 -0400

    making task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
+ * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline."
  * return 0.
  */
@@ -12,5 +12,5 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
-}	
+	return (0);
+}
