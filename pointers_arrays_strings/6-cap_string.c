7e1de09d5b2eed11d299acc5a1d4e87184e29142
Julio <juliojavierbonilla@gmailcom>
2024-10-30 11:07:27 -0400

    making task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,16 +6,16 @@
  * Return: A pointer to the changed string.
  */
 
-char *cap_string(char *str) 
+char *cap_string(char *str)
 
 {
 	int index = 0;
 
 	while (str[index])
 	{
-	while (!(str[index] >= 'a' && str [index] <= 'z'))
+	while (!(str[index] >= 'a' && str[index] <= 'z'))
 	index++;
-        if (str[index - 1] == ' ' ||
+	if (str[index - 1] == ' ' ||
 	str[index - 1] == '\t' ||
 	str[index - 1] == '\n' ||
 	str[index - 1] == ',' ||
@@ -28,9 +28,9 @@ char *cap_string(char *str)
 	str[index - 1] == ')' ||
 	str[index - 1] == '{' ||
 	str[index - 1] == '}' ||
-	index == 0)	
-	str[index] -= 32; 
-        index++;
+	index == 0)
+	str[index] -= 32;
+	index++;
 	}
 	return (str);
 }
