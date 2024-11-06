4037482d1c17ccd75cafec7d4299f6bca5927d93
Julio <juliojavierbonilla@gmailcom>
2024-11-06 09:40:05 -0400

    making task 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -9,13 +9,13 @@
  * Return: Always zero
  */
 
-int main(int argc, char *arg[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", arg[i]);
+		printf("%s\n", argv[i]);
 	}
 
 	return (0);
