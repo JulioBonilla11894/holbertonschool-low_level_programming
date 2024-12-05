f922c1bc2118124d432fac8de779e8d817bf3c9c
JulioBonilla11894 <juliojavierbonilla@gmail.com>
2024-12-05 10:28:54 -0400

    fixing task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@
  *
  * Return: 1 (Success), or -1 (Fail)
  */
-int delete_dnodeint_at_index(dlistint_t **head,unsigned int index)
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *h1;
 	dlistint_t *h2;
